38802137b6f59827f200d78a50e25dd1f061102c
sarthakk24 <sarthak.sachdeva.73@gmail.com>
2023-02-10 22:56:36 +0530

    :sparkles: add basic card styling

--- client/components/cards/cards.tsx
+++ client/components/cards/cards.tsx
@@ -1,3 +1,20 @@
-const Card = () => <div className="text-white">card</div>;
+import ProgressBar from './progressBar';
+
+const Card = () => {
+  return (
+    <div className=" w-[20%] rounded min-w-[250px] shadow-lg m-4 bg-white text-black">
+      <img className="w-full" src="https://v1.tailwindcss.com/img/card-top.jpg" alt="Sunset in the mountains" />
+      <div className="px-5 py-3">
+        <div className="font-bold text-lg mb-2">Meeting Website</div>
+        <p className="text-gray-700 text-base">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis
+          eaque, exercitationem praesentium nihil.
+        </p>
+      </div>
+
+      <ProgressBar progress="9" />
+    </div>
+  );
+};
 
 export default Card;
