44d1d915c1ebe43eb7c7a58e6a10b11547f30542
sarthakk24 <sarthak.sachdeva.73@gmail.com>
2023-02-18 20:11:16 +0530

    :construction: complete form functionality

--- client/components/projects/new.tsx
+++ client/components/projects/new.tsx
@@ -48,68 +48,80 @@ const New = () => {
     <>
       <Formik initialValues={initialValues} validationSchema={submitSchema} onSubmit={onSubmit}>
         <Form className="flex justify-center items-center flex-col text-white">
-          <div className="w-10/12 flex flex-col ">
+          <div className="w-10/12 flex flex-col">
             <h2 className="text-medium-turquoise bg-indigo-dye text-3xl rounded-xl font-semibold py-10">
               New Project !
             </h2>
-            <div className="flex flex-col items-center">
-              <div className="w-full flex justify-between">
-                <div className="flex flex-col w-[45%]">
-                  <p>Project Name</p>
-                  <Field name="name" type="text" className="h-6 p-6" />
-                  <ErrorMessage name="name" />
+            <div className="flex flex-col justify-center items-center">
+              <div className="flex justify-between w-6/12">
+                <div className="flex justify-between w-5/12">
+                  <div className="flex flex-col w-full">
+                    <p>Project Name</p>
+                    <Field name="name" type="text" className="h-6 p-6 bg-gray-600 hover:bg-gray-500 rounded-3xl" />
+                    <ErrorMessage name="name" />
+                  </div>
+                </div>
+
+                <div className="flex justify-between w-5/12">
+                  <div className="flex flex-col w-full">
+                    <p>Version : </p>
+                    <Field name="version" type="text" className="h-6 p-6 bg-gray-600 hover:bg-gray-500 rounded-3xl" />
+                    <ErrorMessage name="version" />
+                  </div>
                 </div>
               </div>
 
-              <div className="w-full flex justify-between">
-                <div className="flex flex-col w-[45%]">
-                  <p>Project Name</p>
-                  <Field name="name" type="text" className="h-6 p-6" />
-                  <ErrorMessage name="name" />
+              <div className="flex justify-between w-6/12">
+                <div className="flex flex-col w-full">
+                  <p>Description : </p>
+                  <Field name="description" type="text" className="h-6 p-6 bg-gray-600 hover:bg-gray-500" />
+                  <ErrorMessage name="description" />
                 </div>
               </div>
 
-              <div className="w-full flex justify-between">
-                <div className="flex flex-col w-[45%]">
-                  <p>Project Name</p>
-                  <Field name="name" type="text" className="h-6 p-6" />
-                  <ErrorMessage name="name" />
+              <div className="flex justify-between w-6/12">
+                <div className="flex flex-col w-full">
+                  <p>Video Link : </p>
+                  <Field name="videoLink" type="text" className="h-6 p-6 bg-gray-600 hover:bg-gray-500" />
+                  <ErrorMessage name="videoLink" />
                 </div>
               </div>
 
-              <div className="w-full flex justify-between">
-                <div className="flex flex-col w-[45%]">
-                  <p>Project Name</p>
-                  <Field name="name" type="text" className="h-6 p-6" />
-                  <ErrorMessage name="name" />
+              <div className="flex justify-between w-6/12">
+                <div className="flex justify-between w-5/12">
+                  <div className="flex flex-col w-full">
+                    <p>Investment Progress</p>
+                    <Field
+                      name="investmentProgress"
+                      type="number"
+                      className="rounded-3xl h-6 p-6 bg-gray-600 hover:bg-gray-500"
+                    />
+                    <ErrorMessage name="investmentProgress" />
+                  </div>
+                </div>
+
+                <div className="flex justify-between w-5/12">
+                  <div className="flex flex-col w-full">
+                    <p>Investment Goal </p>
+                    <Field
+                      name="investmentGoals"
+                      type="number"
+                      className="h-6 rounded-3xl  p-6 bg-gray-600 hover:bg-gray-500"
+                    />
+                    <ErrorMessage name="investmentGoals" />
+                  </div>
                 </div>
               </div>
 
-              <div className="w-full flex justify-between">
-                <div className="flex flex-col w-[45%]">
-                  <p>Project Name</p>
-                  <Field name="name" type="text" className="h-6 p-6" />
-                  <ErrorMessage name="name" />
+              <div className="flex justify-between w-6/12">
+                <div className="flex flex-col w-full">
+                  <p>address </p>
+                  <Field name="address" type="string" className="h-6 p-6 bg-gray-600 hover:bg-gray-500" />
+                  <ErrorMessage name="address" />
                 </div>
               </div>
 
-              <div className="w-full flex justify-between">
-                <div className="flex flex-col w-[45%]">
-                  <p>Project Name</p>
-                  <Field name="name" type="text" className="h-6 p-6" />
-                  <ErrorMessage name="name" />
-                </div>
-              </div>
-
-              <div className="w-full flex justify-between">
-                <div className="flex flex-col w-[45%]">
-                  <p>Investment Goal </p>
-                  <Field name="name" type="text" className="h-6 p-6" />
-                  <ErrorMessage name="name" />
-                </div>
-              </div>
-
-              <button type="submit"> submit</button>
+              <button type="submit">submit</button>
             </div>
           </div>
         </Form>
